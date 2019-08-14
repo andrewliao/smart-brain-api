3d7031cd022efaae2ba1996d995ff23a62c2d1a6
Andrew <andrew.sf.liao@gmail.com>
2019-08-14 01:18:45 -0700

    adding the files

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 's0/\/\P4$$w0rD';
+const someOtherPlaintextPassword = 'not_bacon';
+const cors = require('cors');
+const knex = require('knex');
+
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'andrewliao',
+    password : '',
+    database : 'smart-brain'
+  }
+});
+
+
+db.select('*').from('users').then(data => {
+	console.log(data);
+});
+
+/*
+//header
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+//-------------------------------
+//in function
+const hash = bcrypt.hashSync(password, saltRounds)
+*/
+
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+})
+
+app.listen(3000, ()=> {
+	console.log('app is running on port 3000');
+});
+
+app.post('/signin', signin.handleSignin(db, bcrypt));
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt, saltRounds) });
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
+
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)});
+
