883945988a89449fcd8e95cf499c8c675e6dedb3
Andrew <andrew.sf.liao@gmail.com>
2019-08-31 00:44:55 -0700

    adding database connection

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-asymmetrical-19750',
-    user : 'andrewliao',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
